c3df89802767199db8ad8b8a0c12cfec590d7e19
Deepak Sigdel <deepaksigdel5@gmail.com>
2026-02-02 21:54:03 +0900

    Add files via upload
    Since this is first program ..help me to grow in github

---
+++ 26:2:2_firstprogram.py
@@ -0,0 +1,21 @@
+print ("hello wored")
+print ("my name is deepak sigdel")
+
+if 5 > 2:
+    print("5 is greater then 2")
+    print ("5 is greater than 2")
+print("hello world")
+
+x = 5
+y = "deepak sigdel "
+print (x)
+print (y)
+
+#this is comment in pythoin 
+
+d = "dalli "
+print (d)
+
+print ("this is also printing")
+print ('this is also printing')
+# print (this will give the error)
